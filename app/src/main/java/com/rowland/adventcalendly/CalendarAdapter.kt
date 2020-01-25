c3ae1bf43a2e0422e5fd7819b2fd669e21ea2201
RowlandOti <rowlandotienoo@gmail.com>
2020-01-25 14:13:11 +0300

    chore: use pojo to encapsulate the data. Clean approach

--- app/src/main/java/com/rowland/adventcalendly/CalendarAdapter.kt
+++ app/src/main/java/com/rowland/adventcalendly/CalendarAdapter.kt
@@ -4,12 +4,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.row_calendar.view.card_cell_hidden
+import com.rowland.adventcalendly.data.AdventDay
+
+import kotlinx.android.synthetic.main.row_calendar.view.card_cell
 import kotlinx.android.synthetic.main.row_calendar.view.cell_content_hidden
 import kotlinx.android.synthetic.main.row_calendar.view.cell_content_show
 import java.util.*
 
-class CalendarAdapter(var items: List<Any> = mutableListOf()) :
+class CalendarAdapter(var items: List<AdventDay> = mutableListOf()) :
     RecyclerView.Adapter<CalendarAdapter.CalendarViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalendarViewHolder {
@@ -23,23 +25,25 @@ class CalendarAdapter(var items: List<Any> = mutableListOf()) :
     }
 
     override fun onBindViewHolder(holder: CalendarViewHolder, position: Int) {
-           holder.bind()
+           holder.bind(items[position])
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
 
-    fun addData(list: List<Any>) {
+    fun addData(list: List<AdventDay>) {
         items = list
     }
 
     inner class CalendarViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind() {
-            itemView.card_cell_hidden.setOnClickListener { view: View? ->
+
+        fun bind(adventDay: AdventDay) {
+            itemView.card_cell.setOnClickListener { view: View? ->
                 view!!.cell_content_hidden.visibility = View.VISIBLE
                 view.cell_content_show.visibility = View.GONE
             }
+
 
             val calendar = Calendar.getInstance()
             val minDate = Date(calendar.timeInMillis)
---
+++ app/src/main/java/com/rowland/adventcalendly/data/AdventDay.kt
@@ -0,0 +1,7 @@
+package com.rowland.adventcalendly.data
+
+
+/**
+ * Created by Rowland on 1/25/2020.
+ */
+data class AdventDay(val day: Int, val isRedeemed : Boolean)
